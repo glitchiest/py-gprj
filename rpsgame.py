f378aa346f62b5e6beb392cd2856f0c574170ada
glitchiest <epistulax@tutanota.com>
2022-09-27 14:43:38 +0300

    Add computer can win

--- rpsgame.py
+++ rpsgame.py
@@ -4,11 +4,11 @@ import random
 # Global variables
 choices = ["rock", "paper", "scissors"]  # 0 = rock, 1 = paper, 2 = scissor
 user_score = 0
-
+computer_score = 0
 
 # Function which compares the computers and the users choice and then declares the winner
 def checkResults(user, computer):
-    global user_score  # Use global variable
+    global user_score, computer_score  # Use global variable
 
     if user == "rock":
         # Tie
@@ -17,23 +17,23 @@ def checkResults(user, computer):
 
         # Win
         elif computer == "scissors":
-            print("The computer chose  {}, You chose {}. You WON!".format(computer, user))
+            print("The computer chose {}, You chose {}. You WON!".format(computer, user))
             user_score += 1
 
         # Loss
         else:
             print("The computer chose {}, You chose {}. The computer WON!".format(computer, user))
-
+            computer_score += 1
     elif user == "paper":
         # Win
         if computer == "rock":
-            print("The computer chose  {}, You chose {}. You WON!".format(computer, user))
+            print("The computer chose {}, You chose {}. You WON!".format(computer, user))
             user_score += 1
 
         # Loss
         elif computer == "scissors":
             print("The computer chose {}, You chose {}. The computer WON!".format(computer, user))
-
+            computer_score += 1
         # Tie
         else:
             print("It's a TIE!")
@@ -42,7 +42,7 @@ def checkResults(user, computer):
         # Loss
         if computer == "rock":
             print("The computer chose {}, You chose {}. The computer WON!".format(computer, user))
-
+            computer_score += 1
         # Tie
         elif computer == "scissors":
             print("It's a TIE!")
@@ -55,10 +55,11 @@ def checkResults(user, computer):
 
 # The users choice function
 def userChoice(choices=[]):
-    global user_score  # Use global variable
+    global user_score, computer_score  # Use global variable
     while True:
         try:
-            print("\nCurrent score: {}".format(user_score))
+            print("\nYour score: {}".format(user_score))
+            print("\nThe computer's score: {}".format(computer_score))
             print("\nMake your choice:\n1:Rock\n2:Paper\n3:Scissors")
             user_input = int(input())
             # Check that int is between 1-3
@@ -86,9 +87,16 @@ pointsNeeded = int(input("How many points are required to win? "))
 
 
 # The game starts here
-while user_score < pointsNeeded:
+while user_score < pointsNeeded and computer_score < pointsNeeded:
     user = userChoice(choices)
     computer = computerChoice(choices)
     checkResults(user, computer)
-    if user_score == pointsNeeded:
-        print("\nFinal Score:", user_score)
+    if user_score == pointsNeeded or computer_score == pointsNeeded:
+        if user_score > computer_score:
+            print("\nYour final score:", user_score)
+            print("The computer's final score:", computer_score)
+            print("You WON the game")
+        else:
+            print("\nYour final Score:", user_score)
+            print("The computer's final score:", computer_score)
+            print("You LOST the game")
